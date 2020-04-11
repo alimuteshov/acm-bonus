49d6f69c1c4808415338b74ef7b4fb2936eba1c7
miptstudent <semyonoffss@yandex.ru>
2020-04-11 23:21:27 +0500

    Чтение из файла+таблица

--- working/main.cpp
+++ working/main.cpp
@@ -43,3 +43,86 @@ int Overlap(string s1, string s2)
     }
     return overlap;
 }
+string Prefix(string s1, int ov)
+{
+    return s1.substr(0, s1.length() - ov);
+}
+
+string StringBuilder(vector<string>& super_strings)
+{
+    string s;
+    for (int j = 0; j < super_strings.size(); j++)
+    {
+        s += super_strings[j];
+    }
+    return s;
+}
+
+
+int main()
+{
+    /*string s1;
+    string s2;
+    cin >> s1;
+    cin >> s2;*/
+
+
+    ifstream f("SCSP_in.txt");
+    string str;
+    int n = 0;
+
+    while (getline(f, str))
+    {
+        n++;
+    }
+    f.close();
+
+    cout << n << endl;
+    struct strings {
+        string value;
+    };
+    strings* list = new strings[n];
+
+    ifstream fl("SCSP_in.txt");
+    string s;
+
+    for (int j = 0; j < n; j++)
+    {
+        getline(fl, s);
+        list[j].value = s;
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << list[j].value << endl;
+    }
+
+
+    int** table = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        table[i] = new int[n];
+    }
+
+    int** arr = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                table[i][j] = Overlap(list[i].value, list[j].value);
+            }
+            else
+            {
+                table[i][j] = 0;
+            }
+        }
+    }
+    return 0;
+}
