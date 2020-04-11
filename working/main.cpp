08186269801a6298a70e87232bea0f392b73e20b
alim <alimuteshov@gmail.com>
2020-04-11 21:26:44 +0300

    вывод таблицы

--- working/main.cpp
+++ working/main.cpp
@@ -124,5 +124,18 @@ int main()
             }
         }
     }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
     return 0;
 }
