db9941eb10edb190c45213b3b4148e88b5640f7c
miptstudent <semyonoffss@yandex.ru>
2020-04-12 22:40:52 +0500

    sb

---
+++ Appr_Alg/StringBuilder.h
@@ -0,0 +1,12 @@
+#ifndef StringBuilder_H
+#define StringBuilder_H
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+string StringBuilder(vector<string>& super_strings);
+
+
+#endif
