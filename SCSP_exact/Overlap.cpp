766c4211f6ceb6c3612410fcb54e8876a4aa0ed0
miptstudent <semyonoffss@yandex.ru>
2020-04-21 18:55:56 +0500

    adding

---
+++ SCSP_exact/Overlap.cpp
@@ -0,0 +1,42 @@
+#include "Overlap.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Overlap(string s1, string s2)
+{
+    int overlap = 0;
+    int len = 0;
+    if (s1.length() <= s2.length())
+    {
+        len = s1.length();
+    }
+    else
+    {
+        len = s2.length();
+    }
+    for (int i = 0; i < len; i++)
+    {
+        std::string temp1;
+        for (int j = 0; j <= i; j++)
+        {
+            char temp = s1[s1.length() - 1 - j];
+            temp1 = temp + temp1;
+        }
+        std::string temp2;
+        for (int j = 0; j <= i; j++)
+        {
+            char temp = s2[j];
+            temp2 = temp2 + temp;
+        }
+        if (temp1 == temp2)
+        {
+            if (overlap < i + 1)
+            {
+                overlap = i + 1;
+            }
+        }
+    }
+    return overlap;
+}
---
+++ SCSP_exact/Overlap.h
@@ -0,0 +1,9 @@
+#ifndef Overlap_H
+#define Overlap_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+int Overlap(string s1, string s2);
+
+#endif
